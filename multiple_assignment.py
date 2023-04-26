bc1c8c9772314d61c733e8e58bfb16dbd4a49156
AlexTatro-Github <131911944+AlexTatro-Github@users.noreply.github.com>
2023-04-26 15:31:55 -0500

    Add files via upload

---
+++ multiple_assignment.py
@@ -0,0 +1,19 @@
+#multiple assignment = assign multiple variables at same time with 1 code line
+
+#name =  "Alex"
+#age = 20
+#tired = True
+
+#name, age, tired = "Alex", 20, True
+# Must be separated by commas, in same order
+#print(name)
+#print(age)
+#print(tired)
+
+Monday = Tuesday = Wednesday = Thursday = Friday = True
+
+print("Is Monday boring? " + str(Monday))
+print("Is Tuesday boring? " + str(Tuesday))
+print("Is Wednesday boring? " + str(Wednesday))
+print("Is Thursday boring? " + str(Thursday))
+print("Is Friday boring? " + str(Friday))

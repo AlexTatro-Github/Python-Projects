5759bde0144e90052134b795ba24c7c07c7a716b
AlexTatro-Github <131911944+AlexTatro-Github@users.noreply.github.com>
2023-04-26 14:50:53 -0500

    Add files via upload

---
+++ Variables_Practice.py
@@ -0,0 +1,34 @@
+# Welcome to variable practice
+# string = series of characters like a name
+
+# first_name = "Alex"
+# last_name = "Tatro"
+# full_name = first_name + " " + last_name
+
+# print("hello "+full_name)
+
+# integer = used for math
+
+#age = 20
+#bday_age = age + 1
+#print(bday_age)
+#print(type(bday_age))
+#print("your age is now "+str(bday_age))
+
+# float = decimal point number
+
+#height = 177.8
+
+#print(height)
+#print(type(height))
+
+#print("your height is "+str(height)+"cm")
+
+# boolean = true or false, useful for if - then statements
+
+human = True
+
+print(human)
+print(type(human))
+
+print("are you a human? "+str(human))
